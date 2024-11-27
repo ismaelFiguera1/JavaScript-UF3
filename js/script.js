9ff47f998885aecb3e2174d129babce5734dcba0
ismaelFiguera <ismaelfiguera112@gnail.com>
2024-11-27 19:07:07 +0100

    dia 1

---
+++ js/script.js
@@ -0,0 +1,29 @@
+console.log(navigator);
+console.log(Geolocation);
+console.log(history);
+console.log(location);
+console.log(localStorage);
+console.log(document);
+let text = "Hola, estem a classe de javascript";
+const parlar = (text) => {
+  speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+};
+//  parlar(text);
+
+console.log(document.head);
+console.log(document.body);
+console.log(document.documentElement);
+console.log(document.characterSet);
+console.log(document.links);
+console.log(document.images);
+console.log(document.forms);
+console.log(document.styleSheets);
+console.log(document.scripts);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
